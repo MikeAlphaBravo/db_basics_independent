ad606da25de3dc92040017d085500a806bee889a
Michael Brooks <silverfox@Michaels-MacBook-Pro.local>
2017-09-22 15:01:22 -0700

    add functionality of main page and passing integration spec 1

--- app.rb
+++ app.rb
@@ -6,4 +6,25 @@ also_reload('lib/**/*.rb')
 require('pg')
 require('pry')
 
-DB = PG.connect({:dbhome => "volunteer_tracker_test"})
+DB = PG.connect({:dbname => "volunteer_tracker_test"})
+
+get("/") do
+  @projects = Project.all()
+  erb(:projects)
+end
+
+post("/projects") do
+  title = params["title"]
+  project = Project.new({:title => title, :id => nil})
+  project.save()
+  @projects = Project.all()
+  erb(:projects)
+end
+
+
+
+get("/projects/:id") do
+  @project = Project.find(params.fetch("id").to_i())
+  @volunteers = Volunteer.all()
+  erb(:project_info)
+end
