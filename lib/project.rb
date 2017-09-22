077aa259afcb7c57353c7bc657cbf7040a5d6c9f
Michael Brooks <silverfox@Michaels-MacBook-Pro.local>
2017-09-22 10:29:23 -0700

    add passing and functional all, save and attr test and methods to project

--- lib/project.rb
+++ lib/project.rb
@@ -5,4 +5,25 @@ class Project
     @title = attributes[:title]
     @id = attributes[:id]
   end
+
+  def self.all
+    projects_array = []
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    returned_projects.each do |project|
+      title = project["title"]
+      id = project["id"].to_i
+      projects_array.push(Project.new({:title => title, :id => id}))
+    end
+    projects_array
+  end
+
+  def save
+    input = DB.exec("INSERT INTO projects (title) VALUES ('#{title}') RETURNING id;")
+    @id = input.first["id"].to_i
+  end
+
+  def ==(another_project)
+    title.==(another_project.title).&(id.==(another_project.id))
+  end
+
 end
